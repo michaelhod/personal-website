00b2fc3b0884584bfcf65f6fcbf3d5135be8884b
Michael <michaelhodgins@live.co.uk>
2025-01-13 00:27:21 +0000

    Added the half speed scrolling ability

---
+++ PersonalBlazorWebApp/wwwroot/js/ScrollSpeed.js
@@ -0,0 +1,11 @@
+﻿function parallax() {
+    var s = document.getElementById("floater");
+    var yPos = 0 - window.scrollY / 2;
+    if (window.innerWidth > 350) {
+        s.style.top = 0 - yPos + "px";
+    }
+}
+
+window.addEventListener("scroll", function () {
+    parallax();
+});
