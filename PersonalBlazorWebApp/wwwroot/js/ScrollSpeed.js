d5c2d62f6de2b0b858c1ef713a7bceaeab67d5f3
Michael <michaelhodgins@live.co.uk>
2025-01-14 14:28:48 +0000

    Removed 0s from js code

--- PersonalBlazorWebApp/wwwroot/js/ScrollSpeed.js
+++ PersonalBlazorWebApp/wwwroot/js/ScrollSpeed.js
@@ -1,8 +1,8 @@
 ﻿function parallax() {
     var s = document.getElementById("floater");
-    var yPos = Math.round(0 - window.scrollY/1.1);
+    var yPos = Math.round(window.scrollY/1.1);
     if (window.innerWidth > 550) {
-        s.style.top = 0 - yPos + "px";
+        s.style.top = yPos + "px";
     }
 }
 
