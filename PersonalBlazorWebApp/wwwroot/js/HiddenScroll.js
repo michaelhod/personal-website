d9e06429a8651f127457e116020d4bf3f4386f85
Michael <michaelhodgins@live.co.uk>
2025-03-29 17:17:56 +0000

    Added functionality to disappear text until scrolling. Also made NavLinks default to the top of the page instead of where it previously was

---
+++ PersonalBlazorWebApp/wwwroot/js/HiddenScroll.js
@@ -0,0 +1,15 @@
+﻿// Toggle .hidden based on scroll position
+function handleScroll() {
+    const elements = document.getElementsByClassName("visible-element");
+    if (elements.length === 0) return;
+
+    for (const element of elements) {
+        if (window.scrollY === 0) {
+            element.classList.add("hidden");
+        } else {
+            element.classList.remove("hidden");
+        }
+    }
+}
+
+window.addEventListener("scroll", handleScroll);
--- PersonalBlazorWebApp/wwwroot/js/ScrollSpeed.js
+++ PersonalBlazorWebApp/wwwroot/js/ScrollSpeed.js
@@ -1,12 +1,12 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const parallax = document.getElementById("parallaxImage");
+﻿//document.addEventListener("DOMContentLoaded", function () {
+//    const parallax = document.getElementById("parallaxImage");
 
-    function updateParallax() {
-        let scrollOffset = window.scrollY;
-        let parallaxSpeed = 0.5;
-        parallax.style.transform = `translateY(${scrollOffset * parallaxSpeed}px)`;
-        requestAnimationFrame(updateParallax);
-    }
+//    function updateParallax() {
+//        let scrollOffset = window.scrollY;
+//        let parallaxSpeed = 0.5;
+//        parallax.style.transform = `translateY(${scrollOffset * parallaxSpeed}px)`;
+//        requestAnimationFrame(updateParallax);
+//    }
 
-    updateParallax();
-});
+//    updateParallax();
+//});
